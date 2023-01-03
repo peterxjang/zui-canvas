0fe48ddf1e5b4572ea7c83b3f9609b1abd1e14f3
Peter Jang <peter.x.jang@gmail.com>
2023-01-03 08:00:20 -0600

    Add canvas drawing example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+function initialize() {
+  const $maybeCanvas = document.querySelector("canvas");
+  if (!$maybeCanvas) {
+    return;
+  }
+  const $canvas : HTMLCanvasElement = $maybeCanvas;
+  const maybeCtx = $canvas.getContext("2d");
+  if (!maybeCtx) {
+    return;
+  }
+  const ctx : CanvasRenderingContext2D = maybeCtx;
+  draw($canvas, ctx);
+}
+
+function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "red";
+  ctx.fillRect(10, 10, 50, 50);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(30, 30, 50, 50);  
+}
+
+initialize();
+
 export const Greeter = (name: string) => `Hello ${name}`;
